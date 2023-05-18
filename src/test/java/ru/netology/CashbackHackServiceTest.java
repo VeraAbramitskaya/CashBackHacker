b1feeac652ddd8fa261497bce21e41e4b6f2273f
VeraA <79658000126@ya.ru>
2023-05-18 22:38:13 +0300

    junit4-platform

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -39,5 +39,41 @@ public class CashbackHackServiceTest {
         org.junit.Assert.assertEquals(expected, actual);
     }
 
+    @org.junit.jupiter.api.Test
+
+    public void testRemain500_jupiter() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+
+        int amount = 500;
+        int expected = 500;
+        int actual = cashbackHackService.remain(amount);
+
+        org.junit.jupiter.api.Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+
+    public void testRemainZero_jupiter() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+
+        int amount = 1000;
+        int expected = 0;
+        int actual = cashbackHackService.remain(amount);
+
+        org.junit.jupiter.api.Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+
+    public void testRemainWhenAmountExceedsBoundary_jupiter() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+
+        int amount = 1500;
+        int expected = 500;
+        int actual = cashbackHackService.remain(amount);
+
+        org.junit.jupiter.api.Assertions.assertEquals(expected, actual);
+    }
+
 
 }
